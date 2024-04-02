02ea288060f85787ea57a00201936bca793fc654
LucasLutf <LucasLutf>
2024-04-01 23:58:01 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
+import exprees from "express";
+import { config } from "dotenv";
 
-console.log("hello world!")
+config();
+
+const app = exprees();
+
+const port = process.env.port || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
