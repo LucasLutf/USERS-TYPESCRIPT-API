5be4e90a2331fb5dc7640805fb5157fcf44b146d
LucasLutf <LucasLutf>
2024-04-02 18:07:36 -0300

    feat: add MongoCreateUserRepository

---
+++ src/controllers/create-user/protocols.ts
@@ -0,0 +1,13 @@
+import { User } from "../../models/user";
+
+export interface CreateUserParams {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+}
+
+export interface ICreateUserRepository {
+    createUser(params: CreateUserParams): Promise<User>;
+
+}
