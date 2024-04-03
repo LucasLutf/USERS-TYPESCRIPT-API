80aa47607d2a6b90067421e385d040a3c7c9bc92
LucasLutf <LucasLutf>
2024-04-03 00:19:52 -0300

    feat: add CreateUserController with validations

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,14 @@ import { config } from "dotenv";
 import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
 import { GetUsersController } from "./controllers/get-users/get-users";
 import { MongoClient } from "./database/mongo";
+import { MongoCreateUserRepository } from "./repositories/get-users/create-user/mongo-create-user";
+import { CreateUserController } from "./controllers/create-user/create-user";
 
 const main = async () => {
   config();
   const app = exprees();
+
+  app.use(exprees.json());
 
   await MongoClient.connect();
 
@@ -17,7 +21,20 @@ const main = async () => {
 
     const { body, statusCode } = await getUsersController.handle();
 
-    res.send(body).status(statusCode);
+    res.status(statusCode).send(body);
+  });
+
+  app.post("/users", async (req, res) => {
+    const mongoCreateUserRepository = new MongoCreateUserRepository();
+
+    const createUserController = new CreateUserController(
+      mongoCreateUserRepository
+    );
+
+    const { body, statusCode } = await createUserController.handle({
+      body: req.body,
+    });
+    return res.status(statusCode).send(body);
   });
 
   const port = process.env.PORT || 8000;
